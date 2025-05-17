89ca2d896def7905be40998c9c5d3facbedb9aad
Anton Smirnov <as5200fu@zedat.fu-berlin.de>
2025-05-17 03:26:56 +0200

    Update build_index.py

--- build_index.py
+++ build_index.py
@@ -5,15 +5,15 @@ from langchain_community.vectorstores import FAISS
 from langchain_community.embeddings import OpenAIEmbeddings
 from dotenv import load_dotenv
 
-# === Загрузка .env и API ключа ===
+# .env and API KEY
 load_dotenv()
 assert "OPENAI_API_KEY" in os.environ, "OPENAI_API_KEY not found in environment."
 
-# === Настройки ===
-INPUT_FILE = "/Users/Tosha/Desktop/al-Buhari RAG application/Data/sahih_bukhari_normalized.json"
-OUTPUT_DIR = "/Users/Tosha/Desktop/al-Buhari RAG application/faiss_index"
+#input and putput
+INPUT_FILE = "sahih_bukhari_normalized.json"
+OUTPUT_DIR = "project folder/faiss_index"
 
-# === Загрузка хадисов ===
+#Load of hadith
 def load_documents(path):
     docs = []
     with open(path, 'r', encoding='utf-8') as f:
@@ -24,21 +24,20 @@ def load_documents(path):
             docs.append(Document(page_content=text, metadata=metadata))
     return docs
 
-# ====== ГЛАВНАЯ ФУНКЦИЯ ======
+#main function
 def main():
-    print(f" Загрузка хадисов из {INPUT_FILE}...")
+    print(f" Loading from {INPUT_FILE}...")
     docs = load_documents(INPUT_FILE)
 
-    print(f" Векторизация {len(docs)} хадисов (1 хадис = 1 чанк)...")
+    print(f" Vectorization {len(docs)} hadith (1 hadith = 1 chunk)...")
     embeddings = OpenAIEmbeddings()
     vectorstore = FAISS.from_documents(docs, embeddings)
 
-    print(f" Сохраняю индекс в {OUTPUT_DIR}...")
+    print(f" Saving index in {OUTPUT_DIR}...")
     os.makedirs(OUTPUT_DIR, exist_ok=True)
     vectorstore.save_local(OUTPUT_DIR)
 
-    print(" Индекс готов!")
+    print("Index is done")
 
-# ====== ЗАПУСК ======
 if __name__ == "__main__":
     main()
